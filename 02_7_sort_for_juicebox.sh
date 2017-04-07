b2c09860cf0a1d82505cd730725b67cf2468e839
Ilya Flyamer <flyamer@gmail.com>
2017-04-07 13:51:00 +0100

    Update 02_7_sort_for_juicebox.sh

--- 02_7_sort_for_juicebox.sh
+++ 02_7_sort_for_juicebox.sh
@@ -8,6 +8,6 @@
 
 module load java/jdk/1.8.0
 
-#cat /exports/eddie/scratch/s1529682/processed/merged/for_juicebox/$1\_for_juicebox.txt | sort -k2,2d -k6,6d -n -t $'\t' -T /exports/eddie/scratch/s1529682/tmp/ -S 128G --parallel 8 | uniq | nl -n ln > /exports/eddie/scratch/s1529682/processed/merged/for_juicebox/$1\_for_juicebox_sorted.txt
+cat /exports/eddie/scratch/s1529682/processed/merged/for_juicebox/$1\_for_juicebox.txt | sort -k2,2d -k6,6d -n -t $'\t' -T /exports/eddie/scratch/s1529682/tmp/ -S 128G --parallel 8 | uniq | nl -n ln > /exports/eddie/scratch/s1529682/processed/merged/for_juicebox/$1\_for_juicebox_sorted.txt
 cd /exports/igmm/eddie/wendy-lab/ilia/scripts
 sh juicebox.sh pre /exports/eddie/scratch/s1529682/processed/merged/for_juicebox/$1\_for_juicebox_sorted.txt $1.hic mm9
